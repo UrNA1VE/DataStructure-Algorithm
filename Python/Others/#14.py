96d8ff45671a023e50996f08571c5ccc954d2e2d
UrNA1VE <59864590+UrNA1VE@users.noreply.github.com>
2022-06-27 11:21:44 -0600

    Create #14.py

---
+++ Python/Others/#14.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/longest-common-prefix/
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = []
+        for i in zip(*strs):
+            if len(set(i)) == 1:
+                prefix.append(i[0])
+            else:
+                break
+        return ''.join(prefix)
