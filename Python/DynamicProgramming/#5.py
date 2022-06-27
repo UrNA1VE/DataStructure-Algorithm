cf5301a8dc589b9001883ae399f51938d2096107
UrNA1VE <59864590+UrNA1VE@users.noreply.github.com>
2022-06-27 11:10:49 -0600

    Update #5.py

--- Python/DynamicProgramming/#5.py
+++ Python/DynamicProgramming/#5.py
@@ -2,3 +2,26 @@
 # Input: s = "babad"
 # Output: "bab"
 # Explanation: "aba" is also a valid answer.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        size = len(s)
+        dp = [[False] * size for i in range(size)]
+        res = s[0:1]
+        
+        def helper(i, j):
+            nonlocal res
+            if s[i] == s[j] and (i - j < 2 or dp[j + 1][i - 1]):
+                dp[j][i] = True
+                if len(res) < i - j + 1:
+                    res = s[j: i + 1]
+
+            return
+
+        for i in range(size):
+            dp[i][i] = True
+        for i in range(size):
+            for j in range(i - 1, -1, -1):
+                helper(i, j)
+        return res
+        
