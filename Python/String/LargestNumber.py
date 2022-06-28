95c519fcf3e22cd86b73b827c0a59440ebbaba83
Qiankang Wang <59864590+UrNA1VE@users.noreply.github.com>
2022-06-27 20:41:40 -0600

    Create LargestNumber.py

---
+++ Python/String/LargestNumber.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/largest-number/ 
+
+class Helper(str):
+    def __lt__(x, y):
+        return x+y > y + x
+
+
+class Solution:
+    def largestNumber(self, nums):
+        largest_num = ''.join(sorted(map(str, nums), key=Helper))
+        return '0' if largest_num[0] == '0' else largest_num
