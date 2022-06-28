9be6fbf80df8a72c561fcfa32d3844f964facef6
Qiankang Wang <59864590+UrNA1VE@users.noreply.github.com>
2022-06-27 20:23:52 -0600

    Create FindMinimuminRotatedSortedArrayII.cpp

---
+++ C++/BinarySearch/FindMinimuminRotatedSortedArrayII.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        int mid;
+        while (r > l){
+            mid = (l + r) / 2;
+            if (nums[mid] > nums[r]){
+                l = mid + 1;
+            }
+            else if (nums[mid] < nums[r]){
+                r = mid;
+            }
+            else {
+                r--;
+            }
+        }
+        return nums[l];
+        
+    }
+};
