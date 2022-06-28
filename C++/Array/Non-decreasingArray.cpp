6eb832b700982b4dcbedd885dffc940aaabfd931
Qiankang Wang <59864590+UrNA1VE@users.noreply.github.com>
2022-06-28 15:35:00 -0600

    Create Non-decreasingArray.cpp

---
+++ C++/Array/Non-decreasingArray.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/non-decreasing-array/
+
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int count = 1;
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[i - 1] > nums[i]){
+                if (count == 0) return false;
+                if (i == 1 || nums[i] >= nums[i - 2]) nums[i - 1] = nums[i];
+                else nums[i] = nums[i - 1];
+                count--;
+            }
+        }
+        return true;
+    }
+
+};
