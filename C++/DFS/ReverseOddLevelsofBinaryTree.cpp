ef255dff3eaa1b75a5d6a9084ebf00b676d0dcd3
Qiankang Wang <59864590+UrNA1VE@users.noreply.github.com>
2023-05-23 09:56:41 -0600

    Create ReverseOddLevelsofBinaryTree.cpp

---
+++ C++/DFS/ReverseOddLevelsofBinaryTree.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/reverse-odd-levels-of-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void dfs(TreeNode* l, TreeNode* r, int level) {
+        if (l == nullptr || r == nullptr) return;
+        if (level % 2 == 1) swap(l->val, r->val);
+        dfs(l->left, r->right, level + 1);
+        dfs(l->right, r->left, level + 1);
+    }
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        dfs(root->left, root->right, 1);
+        return root;
+    }
+};
